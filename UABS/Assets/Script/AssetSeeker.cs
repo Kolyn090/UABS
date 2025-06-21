4438117d20a99f211e2b5a9114f387f81c886361
Kolyn090 <kolynlin@protonmail.com>
2025-06-20 21:54:02 -0400

    Initial commit

---
+++ UABS/Assets/Script/AssetSeeker.cs
@@ -0,0 +1,176 @@
+using System;
+using UnityEngine;
+using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+
+public class AssetSeeker : MonoBehaviour
+{
+    public class SpriteInfo
+    {
+        public string Name { get; set; }
+        public long PathId { get; set; }
+    }
+
+    public class Bundle
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public List<SpriteInfo> SpriteInfos { get; set; }
+    }
+
+    private const string GameData = @"\\?\C:\Program Files (x86)\Steam\steamapps\common\Otherworld Legends\Otherworld Legends_Data\StreamingAssets\aa\StandaloneWindows64";
+    private const string SurfCache = "Assets/UABSCache";
+    private void Start()
+    {
+        // List<string> paths = SurfFilesUnderDirExcludeMeta("Assets/TestBundles");
+        // foreach (string path in paths)
+        // {
+        //     Debug.Log(ReadCABCode(path));
+        //     List<(long, string)> pathIDNameOfSprites = GetPathIDNameOfSpritesInBundle(path);
+        //     foreach (var item in pathIDNameOfSprites)
+        //     {
+        //         Debug.Log(item);
+        //     }
+        // }
+
+        if (!Directory.Exists(SurfCache))
+        {
+            Directory.CreateDirectory(SurfCache);
+        }
+        BuildCache(SurfCache);
+    }
+
+    private void BuildCache(string cacheFolder)
+    {
+        List<string> paths = SurfFoldersUnderDirExcludeMeta(GameData);
+        foreach (string path in paths)
+        {
+            string basePath = path.Replace(GameData, "");
+            string newPath = cacheFolder + "\\" + basePath;
+            if (!Directory.Exists(newPath))
+            {
+                Debug.Log($"Creating path {newPath}");
+                Directory.CreateDirectory(newPath);
+            }
+            List<Bundle> writeToIndex = new();
+            List<string> filesInPath = SurfFilesUnderDirExcludeMetaTopDirOnly(path);
+            // Create Bundle objects
+            foreach (string fileInPath in filesInPath)
+            {
+                Bundle bundle = new();
+                bundle.Path = fileInPath;
+                bundle.Name = Path.GetFileName(fileInPath);
+
+                // --- Sprites ---
+                // Debug.Log(fileInPath);
+                bundle.SpriteInfos= GetSpriteInfoInBundle(fileInPath);
+                writeToIndex.Add(bundle);
+            }
+
+            string indexFile = newPath + "\\index.json";
+            File.WriteAllText(indexFile, JsonConvert.SerializeObject(writeToIndex, Formatting.Indented));
+        }
+    }
+
+    private List<string> SurfFoldersUnderDirExcludeMeta(string directory)
+    {
+        List<string> result = new();
+        string[] allFolders = Directory.GetDirectories(directory, "*", SearchOption.AllDirectories);
+
+        foreach (string folder in allFolders)
+        {
+            result.Add(folder);
+        }
+        result.Add(directory);
+
+        return result;
+    }
+
+    private List<string> SurfFilesUnderDirExcludeMeta(string directory)
+    {
+        List<string> result = new();
+        string[] allFiles = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+        foreach (string file in allFiles)
+        {
+            if (file.EndsWith(".bundle"))
+                result.Add(file);
+        }
+
+        return result;
+    }
+
+    private List<string> SurfFilesUnderDirExcludeMetaTopDirOnly(string directory)
+    {
+        List<string> result = new();
+        string[] allFiles = Directory.GetFiles(directory, "*.*", SearchOption.TopDirectoryOnly);
+
+        foreach (string file in allFiles)
+        {
+            if (file.EndsWith(".bundle"))
+                result.Add(file);
+        }
+
+        return result;
+    }
+
+    private List<SpriteInfo> GetSpriteInfoInBundle(string bundlePath)
+    {
+        List<SpriteInfo> result = new();
+        AssetsManager am = new();
+        BundleFileInstance bunInst = am.LoadBundleFile(bundlePath, true);
+        AssetsFileInstance fileInst = am.LoadAssetsFileFromBundle(bunInst, 0, false);
+
+        AssetsFile afile = fileInst.file;
+        List<AssetFileInfo> spriteAssets = afile.GetAssetsOfType(AssetClassID.Sprite);
+
+        if (spriteAssets.Count == 0)
+        {
+            Debug.Log("No Sprite found.");
+            return null;
+        }
+
+        foreach (AssetFileInfo targetAsset in spriteAssets)
+        {
+            AssetTypeValueField spriteBase = am.GetBaseField(fileInst, targetAsset);
+            long pathId = long.Parse(targetAsset.PathId.ToString());
+            string name = spriteBase["m_Name"].AsString;
+            result.Add(new() {Name=name, PathId=pathId});
+        }
+
+        return result;
+    }
+
+    private string ReadCABCode(string bundlePath)
+    {
+        using var fs = new FileStream(bundlePath, FileMode.Open, FileAccess.Read);
+        using var br = new BinaryReader(fs);
+
+        // Skip header, find the CAB string
+        byte[] buffer = new byte[fs.Length];
+        br.Read(buffer, 0, buffer.Length);
+
+        string fullText = Encoding.UTF8.GetString(buffer);
+        int index = fullText.IndexOf("CAB-");
+        if (index >= 0)
+        {
+            int end = index;
+            while (end < fullText.Length && !char.IsWhiteSpace(fullText[end]))
+                end++;
+
+            string cab = fullText.Substring(index, end - index);
+            return "CAB code: " + cab;
+        }
+        else
+        {
+            return "";
+        }
+    }
+}
